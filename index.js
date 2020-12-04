e17199d2753d4e38427a1b7c12ef562a420e46ca
Joe Felix <73597256+JoeFelx@users.noreply.github.com>
2020-12-04 19:21:48 +0530

    Simply Drum Game

---
+++ index.js
@@ -0,0 +1,49 @@
+for (i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+            var buttonClicked = this.innerHTML;
+            makeSound(buttonClicked);
+        })
+    }
+
+    document.addEventListener("keydown", function (event) {
+        makeSound(event.key);
+    })
+
+function makeSound(key) {
+    switch (key) {
+        case "a":
+            var a = new Audio("/sounds/tom-1.mp3")
+            a.play();
+            break;
+    
+        case "s":
+            var s = new Audio("/sounds/tom-2.mp3")
+            s.play();         
+            break;
+    
+        case "d":
+            var d = new Audio("/sounds/tom-3.mp3")
+            d.play();
+            break;
+    
+        case "f":
+            var f = new Audio("/sounds/tom-4.mp3")
+            f.play();
+            break;
+
+        case "j":
+            var j = new Audio("/sounds/crash.mp3")
+            j.play();
+            break;
+
+        case "k":
+            var k = new Audio("/sounds/kick-bass.mp3")
+            k.play();
+            break;
+        
+        case "l":
+            var l = new Audio("/sounds/snare.mp3")
+            l.play();
+            break;
+    }
+}
